f7d5cfefb04b83d0bb92fb272cfe31722282805d
Ori Bermudez <ori.bermudez@outlook.com>
2023-11-06 15:17:39 -0700

    Sign up page

---
+++ app/material-tailwind.js
@@ -0,0 +1,19 @@
+"use client"
+
+import {
+  Input,
+  Card,
+  Checkbox,
+  Button,
+  Typography,
+  ThemeProvider,
+} from "@material-tailwind/react";
+
+export  {
+  Input,
+  Card,
+  Checkbox,
+  Button,
+  Typography,
+  ThemeProvider,
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,11 +10,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'login': "url('/assets/background.png')",
       },
+      colors: {
+        emerald: "#04CCC0",
+        royal: "#211A6C"
+      }
     },
   },
   plugins: [],
-}
+});
