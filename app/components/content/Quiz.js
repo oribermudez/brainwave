bdfc20a579bc6c8781da8cceef2c7a7f4b417fd1
Ori Bermudez <ori.bermudez@outlook.com>
2023-11-23 13:52:57 -0700

    Improved Quizzes

--- app/components/content/Quiz.js
+++ app/components/content/Quiz.js
@@ -14,14 +14,14 @@ const Quiz = ({ question, options, correctOption }) => {
   };
 
   return (
-    <div className="p-4 border rounded-lg border border-royal text-black w-full my-8">
+    <div className="p-4 rounded-lg border border-royal text-black w-full my-8">
       <span className='p-3 m-4 font-bold text-royal'>{question}</span>
       <div className={`relative`}>
         {options?.length && options.map((option, index) => (
           <div
             key={index}
             onClick={() => handleAnswerClick(option)}
-            className={`flex items-center p-3 m-4 border border-gray-300 cursor-pointer rounded-lg hover:bg-emerald/20 ${selectedAnswer === option && !isCorrect ? 'animate-shake' : ''}`}
+            className={`flex items-center p-3 m-4 border border-gray-300 cursor-pointer rounded-lg hover:bg-emerald/20 ${selectedAnswer === option && !isCorrect ? 'animate-shake' : ''} ${selectedAnswer === option && isCorrect ? 'bg-emerald/20' : ''}`}
           >
             {`${String.fromCharCode(97 + index)}. ${option}`}
             <Image src="/assets/checked.png" width={25} height={25} alt={'Check Icon'} className={`ml-4 h-6 w-6 ${selectedAnswer === option && isCorrect ? 'block' : 'hidden'}`} />
